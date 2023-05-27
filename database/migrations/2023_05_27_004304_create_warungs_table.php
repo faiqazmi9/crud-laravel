e40a70a4fd351e1fedc7dcbe0630a410149ac4a2
faiqazmi9 <faiqazmi9@gmail.com>
2023-05-27 07:48:59 +0700

    menambahkan tabel dengan migrate

---
+++ database/migrations/2023_05_27_004304_create_warungs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('warung', function (Blueprint $table) {
+            $table->integer('id');
+            $table->unique('id');
+            $table->string('jenis');
+            $table->string('nama');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('warung');
+    }
+};
